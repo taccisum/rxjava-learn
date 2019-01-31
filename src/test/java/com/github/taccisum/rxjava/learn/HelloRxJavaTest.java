762c45a291ae5f70520d8b0233a7e42bb4d01895
taccisum <514162920@qq.com>
2019-01-31 13:58:28 +0800

    simple test

--- src/test/java/com/github/taccisum/rxjava/learn/HelloRxJavaTest.java
+++ src/test/java/com/github/taccisum/rxjava/learn/HelloRxJavaTest.java
@@ -1,7 +1,10 @@
 package com.github.taccisum.rxjava.learn;
 
 import io.reactivex.Flowable;
+import io.reactivex.Observable;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 /**
  * @author tac
@@ -12,5 +15,56 @@ public class HelloRxJavaTest {
     public void name() throws Exception {
         Flowable.just("Hello world")
                 .subscribe(o -> System.out.println(o + "!"));
+
+        Observable.just(1L, 2L, "hello").subscribe(o -> {
+            System.out.println(Thread.currentThread());
+            System.out.println(o.getClass());
+            System.out.println(o);
+        });
+        Observable.fromCallable(() -> Arrays.asList(1L, 2L)).subscribe(System.out::println);
+        Observable.fromArray(1L, 2L, 3L).subscribe(System.out::println);
+
+        Observable.create(aSubscriber -> {
+            for (int i = 0; i < 5; i++) {
+                if (!aSubscriber.isDisposed() && i % 2 == 0) {
+                    aSubscriber.onNext("value_" + i);
+                }
+            }
+
+            if (!aSubscriber.isDisposed()) {
+                aSubscriber.onComplete();
+            }
+
+            for (int i = 0; i < 5; i++) {
+                if (!aSubscriber.isDisposed() && i % 2 == 0) {
+                    aSubscriber.onNext("value_" + i);
+                }
+            }
+
+            // how to use onError()
+//            aSubscriber.onError(new RuntimeException());
+
+        })
+                .doOnSubscribe(o -> System.out.println("subscribed"))
+                .doOnComplete(() -> System.out.println("completed"))
+                .subscribe(System.out::println);
+
+        // async
+        Observable.create(aSubscriber -> {
+            new Thread(() -> {
+                for (int i = 0; i < 5; i++) {
+                    if (!aSubscriber.isDisposed()) {
+                        aSubscriber.onNext("value_" + i);
+                    }
+                }
+
+                if (!aSubscriber.isDisposed()) {
+                    aSubscriber.onComplete();
+                }
+            }).start();
+
+        }).subscribe(System.out::println);
+
+        System.out.println("hhh");
     }
 }
